91ca1ea78ea9dc73fbd7e85effd48633507b38f4
kimdg1105 <kimss7334@naver.com>
2021-07-25 23:01:56 +0900

    21.07.25 - 주문 검색 기능, 웹 계층 개발(홈 화면)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,24 +1,103 @@
 package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
     private final EntityManager em;
 
-    public void save(Order order){
+    public void save(Order order) {
         em.persist(order);
     }
 
-    public Order findOrder(Long id){
+    public Order findOrder(Long id) {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch){ }
+    /**
+     * JPQL을 이용한 문자열 방법의 동적쿼리는 다음과 같이 매우 복잡하다.
+     * 이는 이후, QueryDSL 등의 기술로 해결한다.
+     *
+     * @param orderSearch
+     * @return
+     */
+    public List<Order> findAll(OrderSearch orderSearch) {
 
+        //language=JPAQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000); //최대 1000건
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+    }
+
+    /**
+     * Criteria를 이용한 동적 쿼리 방법
+     *
+     * @param orderSearch
+     * @return
+     */
+//    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+//        Root<Order> o = cq.from(Order.class);
+//        Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
+//        List<Predicate> criteria = new ArrayList<>();
+////주문 상태 검색
+//        if (orderSearch.getOrderStatus() != null) {
+//            javax.persistence.criteria.Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+//            criteria.add((Predicate) status);
+//        }
+////회원 이름 검색
+//        if (StringUtils.hasText(orderSearch.getMemberName())) {
+//            Predicate name =
+//                    (Predicate) cb.like(m.<String>get("name"), "%" +
+//                            orderSearch.getMemberName() + "%");
+//            criteria.add(name);
+//
+//        }
+//        cq.where(cb.and((javax.persistence.criteria.Predicate) criteria.toArray(new Predicate[criteria.size()])));
+//        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
+//        return query.getResultList();
+//    }
 }
