aa935ab8382b3e79c3445a45670bd33048afd7e8
kimdg1105 <kimss7334@naver.com>
2021-07-26 17:47:10 +0900

    21.07.25 - 회원 등록/수정/조회 API 제작

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,18 +1,21 @@
 package jpabook.jpashop.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Builder
 @Getter // getter만 만든 이유? -> 생성할 때만 값을 넣고, 값의 중간 수정이 불가능하게 설계
 public class Address {
     private String city;
     private String street;
     private String zipcode;
 
-    protected Address() {
+    public Address() {
     }
+
 
     public Address(String city, String street, String zipcode) {
         this.city = city;
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,11 +1,13 @@
 package jpabook.jpashop.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +20,13 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
     private Address address;
 
+//    @JsonIgnore 엔티티에 프레젠테이션 계층을 위한 코드가 들어가는 것은 좋지 않다.
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
