27358647c2c60768378af748799a7934afea2b86
kimdg1105 <kimss7334@naver.com>
2021-08-01 17:41:59 +0900

    21.08.01- SpringDataJPA test

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -36,7 +36,7 @@ public class MemberService {
 
     @Transactional
     public void update(Long id,String name){
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
 
     }
@@ -57,7 +57,7 @@ public class MemberService {
 
     // 회원 단건 조회
     public Member findOneMember(Long memberId){
-        return memberRepository.findOne(memberId);
+        return memberRepository.findById(memberId).get();
     }
 
 
